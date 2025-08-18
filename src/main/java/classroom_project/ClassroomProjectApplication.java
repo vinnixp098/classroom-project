762e0cac5448ae3cb22b57a885c20d4a216aa7cf
Vinícius <vinicius.pires@apotiguar.com.br>
2025-08-18 12:54:32 -0300

    Subindo projeto

---
+++ src/main/java/classroom_project/ClassroomProjectApplication.java
@@ -0,0 +1,13 @@
+package classroom_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomProjectApplication.class, args);
+	}
+
+}
